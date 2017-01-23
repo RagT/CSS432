7c7cc86c3d72edec457317fc364ee2dc2e2ac5fb
RagT <raghutir@gmail.com>
2017-01-22 18:31:56 -0800

    Added client.cpp and server.cpp

---
+++ Client.cpp
@@ -0,0 +1,152 @@
+#include <sys/types.h>    // socket, bind
+#include <sys/socket.h>   // socket, bind, listen, inet_ntoa
+#include <netinet/in.h>   // htonl, htons, inet_ntoa
+#include <arpa/inet.h>    // inet_ntoa
+#include <netdb.h>        // gethostbyname
+#include <unistd.h>       // read, write, close
+#include <string.h>       // bzero
+#include <netinet/tcp.h>  // SO_REUSEADDR
+#include <sys/uio.h>      // writev
+#include <sys/time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+using namespace std;
+
+const int BUFSIZE = 1500;
+
+int main(int argc, char * argv[]) {
+	//Check validity of command line arguments
+
+	if(argc != 7) {
+		cerr << "Incorrect number of arguments." << endl;
+		cerr << endl;
+		cerr << "There were 6 arguments expected." << endl;
+		cerr << "port 			The port number to connect to" << endl;
+		cerr << "repetitions 	The number of times to send a set of data buffers" << endl;
+		cerr << "nbufs 			The number of data buffers" << endl;
+		cerr << "bufsize   		the size of each data buffer (in bytes)" << endl;
+		cerr << "serverIp 		A server Ip name" << endl;
+		cerr << "types 			type of transfer scenario: 1,2,or 3" << endl;
+		cerr << endl;
+	}
+
+	int port = atoi(argv[1]);
+	int repetitions = atoi(argv[2]);
+	int nbufs = atoi(argv[3]);
+	int bufsize = atoi(argv[4]);
+	const char * serverIp = argv[5];
+	int type = atoi(argv[6]);
+
+	if(port < 1024 || port > 65536) {
+		cerr << "Port not in allowable port range between 1024 and 65536" << endl;
+		return -1;
+	} 
+
+	if(repetitions < 0){
+		cerr << "Must have positive number of repetitions" << endl;
+		return -1;
+	}
+
+	if(nbufs * bufsize != BUFSIZE) {
+		cerr << "nbufs * bufsize must equal 1500" << endl;
+		return -1;
+	}
+
+	if(type < 1 || type > 3) {
+		cerr << "Type must be 1,2, or 3" << endl;
+		return -1;
+	}
+
+	struct hostent* host = gethostbyname(serverIp);
+	if(host == NULL) {
+		cerr << "Invalid hostname" << endl;
+		return -1;
+	}
+
+	sockaddr_in sendSockAddr;
+	bzero((char*)sendSockAddr, sizeOf(sendSockAddr));
+	sendSockAddr.sin_family = AF_INET; // Address Family Internet
+    sendSockAddr.sin_addr.s_addr =
+    	inet_addr( inet_ntoa( *(struct in_addr*)*host->h_addr_list ) );
+    sendSockAddr.sin_port = htons( server_port );
+
+    //Open a stream oriented socket with Internet addresss family
+    int clientSd = socket(AF_INET, SOCK_STREAM, 0);
+    if(clientSd < 0) {
+    	cerr << "Could not open a socket" << endl;
+    	close(clientSd);
+    	return -1;
+    }
+
+    //Connect socket to the server
+    if(connect(clientSd, (sockaddr*)&sendSockAddr, sizeOf(sendSockAddr)) < 0) {
+    	cerr << "Failed to connect to server" << endl;
+    	close(clientSd);
+    	return -1;
+    }
+
+    //Allocate databuffer
+    char databuffer[nbufs][bufsize];
+
+    //Start a timer
+    struct timeval start;
+    struct timeval end;
+    struct timeval lap;
+    long lapTime;
+    long roundTripTime;
+
+    //Start time
+    gettimeofday(&start, NULL);
+
+    for(int i = 0; i < repetitions; i++) {
+    	switch(type){
+    		//Multiple writes: invokes the write( ) system call for each data buffer,
+    		//thus resulting in calling as many write( )s as the number of data buffers, 
+    		//(i.e., nbufs).
+    		case 1:
+    			for(int j = 0; j < nbufs; j++) {
+    				write(clientSd, databuffer[j], bufsize);
+    			}
+    		break;
+    		//writev:  allocates an array of iovec data structures, each having its 
+    		//*iov_base field point to a different data buffer as well as storing the buffer size in its iov_len field;
+    		//and thereafter calls writev( ) to send all data buffers at once.
+    		case 2:
+    			struct iovec vector[nbufs];
+			     for (int j = 0; j < nbufs; j++) {
+			       vector[j].iov_base = databuffer[j];
+			       vector[j].iov_len = bufsize;
+			     }
+			     writev(clientSd, vector, nbufs);
+    		break;
+    		//single write: allocates an nbufs-sized array of data buffers, 
+    		//and thereafter calls write( ) to send this array, (i.e., all data buffers) at once.
+    		case 3:
+    			write(clientSd, databuf, nbufs * bufsize); 
+    		break;
+    	}
+    }
+
+    //write lap time
+    gettimeofday(&lap, NULL);
+
+    //Recieve acknowledge from server on how many times read was called
+    int readCount;
+    read(clientSd, &count, sizeOf(readCount));
+
+    //Record round trip end time (RTT)
+    gettimeofday(&end, NULL);
+
+    //Calculate lap and round trip time
+    lapTime = (lap.tv_sec - start.tv_sec) * 1000000 + (lap.tv_usec - start.tv_usec);
+	roundTripTime = (end.tv_sec - start.tv_sec) * 1000000 + (end.tv_usec - start.tv_usec);
+
+    //Print out stats to terminal
+    cout << "Test " << type << ": data-sending time = " << lapTime << " usec, ";
+    cout << "round-trip time = " << roundTripTime << " usec, " << "#reads = " << readCount << endl;
+
+    //close the socket
+    close(clientSd);
+    return 0;
+}
---
+++ Server.cpp
@@ -0,0 +1,128 @@
+#include <sys/types.h>    // socket, bind
+#include <sys/socket.h>   // socket, bind, listen, inet_ntoa
+#include <netinet/in.h>   // htonl, htons, inet_ntoa
+#include <arpa/inet.h>    // inet_ntoa
+#include <netdb.h>        // gethostbyname
+#include <unistd.h>       // read, write, close
+#include <string.h>       // bzero
+#include <netinet/tcp.h>  // SO_REUSEADDR
+#include <sys/uio.h>      // writev
+#include <sys/time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+using namespace std;
+
+const int BUFSIZE = 1500;
+int repetitions = -1;
+int port = -1;
+int serverSd; //Socket descriptor
+int newSd;	//connection request specific socket descriptor
+
+void handlerFunction(int arg) {
+	char dataBuff[BUFSIZE];
+
+	struct timeval start;
+	struct timeval end;
+	long timeElapsed;
+
+	//Start the timer
+	gettimeofday(&start, NULL);
+
+	int count = 0;
+	//Get number of reads
+	for(int i = 0; i < repetitions; i++) {
+		int nRead = 0;
+		while(nRead < BUFSIZE) {
+			nRead += read(fd, dataBuff, BUFSIZE - nRead);
+			count++;
+		}
+	}
+
+	//End the timer
+	gettimeofday(&end, NULL);
+
+	//Calculate time elapsed
+	timeElapsed = (end.tv_sec - start.tv_sec) * 1000000 + (end.tv_usec - start.tv_usec);
+	cout << "data-receiving time =  " << timeElapsed << " usec" << endl;
+
+	//Send number of read() calls back to the client as an acknowledge
+	write(newSd, &count, sizeof(count));
+
+	//Stop server process
+	close(newSd);
+	close(serverSd);
+	exit(0);
+}
+
+int main(int argc, char * argv[]) {
+	//Check validity of command line arguments
+
+	if(argc != 3) {
+		cerr << "Invalid number of arguments." << endl;
+		cerr << endl;
+		cerr << "There were two arguments expected:" << endl;
+		cerr << "port# 			The ip port for server to use." << endl;
+		cerr << "repetitions 	The number of times to send a set of data buffers." << endl;
+		return -1;
+	}
+
+	port = atoi(argv[1]);
+	repetitions = atoi(argv[2]);
+
+	if(port < 1024 || port > 65536) {
+		cerr << "Port outside of allowable port range of 1024 to 65536." << endl;
+		return -1;
+	} 
+
+	if(repetitions < 0){
+		cerr << "Number of repetitions must be positive." << endl;
+		return -1;
+	}
+
+	sockaddr_in acceptSockAddr;
+	bzero((char*)acceptSockAddr, sizeof(acceptSockAddr));
+	acceptSockAddr.sin_family = AF_INET;
+	acceptSockAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	acceptSockAddr.sin_port = htons(port);
+
+	//Open stream oriented socket with Internet address family
+	serverSd = socket(AF_INET, SOCK_STREAM, 0); 
+	if(serverSd < 0) {
+		cerr << "Cannot open a server TCP socket." << endl;
+		return -1;
+	}
+
+	//prompt OS to release server port as soon as process terminates
+	const int on = 1;
+	setsockopt(serverSd, SOL_SOCKET, SO_REUSEADDR, (char*)&on, sizeof(int));
+
+	//Bind socket to local address
+	if(bind(serverSd, (sockaddr *)&acceptSockAddr, sizeof(&acceptSockAddr)) < 0) {
+		cerr << "Unable to bind socket to local address" << endl;
+		close(serverSd);
+		return -1;
+	}
+
+	//Instruct OS to listen to up to fve connection requests from clients
+	listen(serverSd, 5);
+
+	//Recieve a request from a client by calling accept that will return a new socket
+	//specific to this connection request.
+	sockaddr_in newSockAddr;
+    socklen_t newSockAddrSize = sizeof(newSockAddr);
+    int newSd = accept( serverSd, (sockaddr*)&newSockAddr, &newSockAddrSize);
+
+    signal(SIGIO, handlerFunction);
+
+    //Change socket to asynchronous connection
+    fcntl(newSd, F_SETOWN, getpid());
+    fcntl(newSd, F_SETFL, FASYNC);
+
+    while(true) {
+    	sleep(1000);
+    }
+    return 0;
+}
+
+
